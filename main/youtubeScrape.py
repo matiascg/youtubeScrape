905d8641177a5f914c64ed658671aca7a7330a51
Matias <m.carballogonzalez@stud.unibas.ch>
2020-07-29 11:12:54 +0200

    Implemented chanel_video_scraper method

--- main/youtubeScrape.py
+++ main/youtubeScrape.py
@@ -6,65 +6,6 @@ from youtube_transcript_api import YouTubeTranscriptApi as yta
 import pymysql
 from sqlalchemy import create_engine
 
-def video_scrape(url, driver):
-    # Setup
-    driver.get(url)
-    driver.execute_script("window.scrollTo(0, 500);")
-    driver.implicitly_wait(10)
-    data = {}
-
-    # Scrape channel id
-    channel = driver.find_element_by_xpath('//*[@id="text"]/a')
-    link = channel.get_attribute("href")[32:]
-    data['Channel ID'] = link
-    data['Channel name'] = channel.text
-
-    # Scrape Title
-    headlines = driver.find_elements_by_xpath('//*[@id="container"]/h1/yt-formatted-string')
-    data['Video ID'] = url[32:]
-    data['Video Title'] = headlines[0].text
-
-    # Scrape views
-    views = driver.find_element_by_xpath('//*[@id="count"]/yt-view-count-renderer/span[1]')
-    number = int(views.text[:-8].replace(".", ""))
-    data['Views'] = number
-
-    # Scrape likes and dislikes
-    rating = driver.find_elements_by_xpath('//*[@id="text"]')
-    likes = int(rating[4].text.replace(".", ""))
-    dislikes = int(rating[5].text.replace(".", ""))
-    data['Likes'] = likes
-    data['Dislikes'] = dislikes
-
-    # Scrape date
-    dat = driver.find_element_by_xpath('//*[@id="date"]/yt-formatted-string')
-    date_str = ''.join(ch for ch in dat.text if ch.isdigit())
-    date_dat = dt.datetime(int(date_str[-4:]), int(date_str[2:-4]), int(date_str[0:2]))
-    data['Upload Date'] = date_dat.isoformat()
-
-    # Scrape length in seconds
-    length = driver.execute_script(
-        "return document.getElementById('movie_player').getDuration()")
-    data['Duration'] = int(length)
-
-    # Scrape number of comments
-    driver.implicitly_wait(10)
-    comments = driver.find_element_by_xpath('//*[@id="count"]/yt-formatted-string')
-    comment_number = int(''.join(ch for ch in comments.text if ch.isdigit()))
-    data['Number of Comments'] = comment_number
-
-    # Scrape captions
-    # Create a new file
-    f = open("C:\\Users\\matia\\youtubeScrape\\Captions\\" + url[32:] + '.txt', 'w+')
-
-    # Scrape and write into file
-    out, _ = yta.get_transcripts([url[32:]])
-    text_out = ' '.join([x.get('text', '') for x in out[url[32:]]])
-    f.write(text_out)
-    f.close()
-
-    return data
-
 
 def channel_list_creator(driver):
     driver.get('https://socialblade.com/youtube/top/5000/mostsubscribed')
@@ -99,10 +40,74 @@ def channel_list_creator(driver):
     return data
 
 
+def video_scrape(video, driver):
+    # Setup
+    url = 'https://youtube.com/watch?v=' + video
+    driver.get(url)
+    driver.execute_script("window.scrollTo(0, 1000);")
+    driver.implicitly_wait(10)
+    data = {}
+
+    # Scrape channel id
+    channel = driver.find_element_by_xpath('//*[@id="text"]/a')
+    link = channel.get_attribute("href")[32:]
+    data['Channel ID'] = [link]
+
+    # Scrape Title
+    headlines = driver.find_elements_by_xpath('//*[@id="container"]/h1/yt-formatted-string')
+    data['Video ID'] = [video]
+    data['Video Title'] = [headlines[0].text]
+
+    # Scrape views
+    views = driver.find_element_by_xpath('//*[@id="count"]/yt-view-count-renderer/span[1]')
+    number = int(views.text[:-8].replace(".", ""))
+    data['Views'] = [number]
+
+    # Scrape likes and dislikes
+    rating = driver.find_elements_by_xpath('//*[@id="text"]')
+    likes = int(rating[4].text.replace(".", ""))
+    dislikes = int(rating[5].text.replace(".", ""))
+    data['Likes'] = [likes]
+    data['Dislikes'] = [dislikes]
+
+    # Scrape date
+    dat = driver.find_element_by_xpath('//*[@id="date"]/yt-formatted-string')
+    date_str = ''.join(ch for ch in dat.text if ch.isdigit())
+    date_dat = dt.datetime(int(date_str[-4:]), int(date_str[2:-4]), int(date_str[0:2]))
+    data['Upload Date'] = [date_dat.isoformat()]
+
+    # Scrape length in seconds
+    length = driver.execute_script(
+        "return document.getElementById('movie_player').getDuration()")
+    data['Duration'] = [int(length)]
+
+    # Scrape number of comments
+    driver.implicitly_wait(10)
+    comments = driver.find_element_by_xpath('//*[@id="count"]/yt-formatted-string')
+    comment_number = int(''.join(ch for ch in comments.text if ch.isdigit()))
+    data['Number of Comments'] = [comment_number]
+
+    # Scrape captions
+    # Create a new file
+    f = open("C:\\Users\\matia\\youtubeScrape\\Captions\\" + video + '.txt', 'w+')
+
+    # Scrape and write into file
+    # if not yta.get_transcripts(video) is None:
+    out, _ = yta.get_transcripts([video])
+    text_out = ' '.join([x.get('text', '') for x in out[video]])
+    f.write(text_out)
+    f.close()
+
+    vid = pd.DataFrame(data)
+    return vid
+
+
+
+
+
 def channel_scraper(channel, driver):
     # Set up
     url = 'https://youtube.com/channel/' + channel['Channel ID'] + '/videos'
-    video_info = {'Video ID': [], 'Title': []}
     driver.get(url)
     driver.implicitly_wait(10)
 
@@ -112,7 +117,7 @@ def channel_scraper(channel, driver):
     # Adding Subscribers
     abos = str(driver.find_element_by_xpath('//*[@id="subscriber-count"]').text)
 
-    # Translating to int
+    # Translating Subscribers to int
     if 'Mio' in abos:
         abos = abos.partition('Mio')[0]
         if ',' in abos:
@@ -125,13 +130,6 @@ def channel_scraper(channel, driver):
         abos_number = int(abos)
     channel_info['Subscribers'] = [abos_number]
 
-    # Adding Video ID and Title
-    videos = driver.find_elements_by_xpath('//*[@id="video-title"]')
-    for video in videos:
-        video_info['Title'].append(video.text)
-        video_info['Videos'].append(str(video.get_attribute('href')).split('watch?v=', 1)[1])
-
-
     # Getting total views
     driver.get('https://youtube.com/channel/' + channel['Channel ID'] + '/about')
     driver.implicitly_wait(5)
@@ -139,18 +137,33 @@ def channel_scraper(channel, driver):
     views = str(views_ele.text)
     views = views.partition('Auf')[0]
     views_numb = int(views.replace('.', ''))
-    channel_info['Views'] = views_numb
+    channel_info['Views'] = [views_numb]
 
     # Creating a DataFrame
     chan = pd.DataFrame(channel_info)
-    vid = pd.DataFrame(video_info)
-    return chan, vid
+    return chan
+
+
+def channel_vid_scrape(channel, driver):
+    url = 'https://youtube.com/channel/' + channel['Channel ID'] + '/videos'
+    driver.implicitly_wait(5)
+    vid_data = pd.DataFrame(columns=['Video ID', 'Video Title', 'Channel ID', 'Views', 'Likes', 'Dislikes',
+                                     'Upload Date', 'Duration', 'Number of Comments'])
+    driver.get(url)
+    lst = []
+    videos = driver.find_elements_by_xpath('//*[@id="video-title"]')
+    for video in videos:
+        lst = lst.append((str(video.get_attribute('href')).split('watch?v=', 1)[1]))
+    for id in lst:
+        vid_data = vid_data.append(video_scrape(id, driver))
+
+    return vid_data
 
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', -1)
+pd.set_option('display.max_colwidth', None)
 
 
 def multi_scrape(lst, driver):
@@ -160,9 +173,9 @@ def multi_scrape(lst, driver):
     for i in range(len(lst[key])):
         val1, val2 = lst[key][i], lst['Channel ID'][i]
         channel = {key: val1, 'Channel ID': val2}
-        tabchan, tabvid = channel_scraper(channel, driver)
-        tab = tab.append(, ignore_index=True)
-    return tab
+        tab = tab.append(channel_scraper(channel, driver), ignore_index=True)
+        vid = channel_vid_scrape(channel, driver)
+    return tab, vid
 
 
 driver = webdriver.Chrome()
@@ -182,5 +195,7 @@ engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
                                pw=password,
                                db="youtube"))
 
-table.to_sql("youtubers", engine, if_exists='replace')
+tablechan.to_sql("youtubers", engine, if_exists='replace')
+tablevid.to_sql("videos", engine, if_exists='replace')
 print(pd.read_sql_query('select * from youtubers;', conn))
+print(pd.read_sql_query('select * from videos;', conn))
